39ce198591657495e613f299f7966870f09dce05
llcoolj411 <78896384+llcoolj411@users.noreply.github.com>
2021-02-13 00:43:12 +0900

    scriptchange

--- Assets/test.cs
+++ Assets/test.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void hogehoge()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
