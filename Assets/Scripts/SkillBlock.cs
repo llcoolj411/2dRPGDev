e1dcda2930b9c49cd0e910da4d93b501762cd489
llcoolj411 <78896384+llcoolj411@users.noreply.github.com>
2021-02-16 23:35:11 +0900

    skilltree

---
+++ Assets/Scripts/SkillBlock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillBlock : MonoBehaviour
+{
+    SkillType skillType;
+    [SerializeField] int cost;
+    [SerializeField] new string name;
+    [SerializeField] string info;
+
+    public void OnClick()
+    {
+        //習得済みなら何もしない
+        if (SkillManager.instance.HasSkill(this.skillType))
+        {
+            Debug.Log("習得済みです");
+            return;
+        }
+
+        //習得可能か（すでにスキルを持っていないか）
+
+        //習得可能なら習得する:スキルポイントが足りている＆前提スキルを持っている
+        if (SkillManager.instance.CanLearnSkill(cost,skillType))
+        {
+            SkillManager.instance.SkillLearn(this.skillType);
+            Debug.Log("習得！");
+        }
+        else
+        {
+            Debug.Log("習得NG");
+        }
+        //習得不可能ならログを出す
+
+
+    }
+}
---
+++ Assets/Scripts/SkillManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum SkillType
+{
+    ATTACK,
+    DEFFENCE,
+    SPEED1,
+    SPEED2,
+    COMBO,
+    COMBO2
+}
+
+
+public class SkillManager : MonoBehaviour
+{
+    int skillpoint;
+    List<SkillType> SkillList = new List<SkillType>();
+
+    public static SkillManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+
+    public bool HasSkill(SkillType skillType)
+    {
+        return SkillList.Contains(skillType);
+    }
+
+    public bool CanLearnSkill(int cost,SkillType skillType)
+    {
+        if (skillpoint < cost)
+        {
+            return false;
+        }
+
+        if (skillType == SkillType.SPEED2)
+        {
+            return HasSkill(SkillType.SPEED1);
+        }
+        return true;
+    }
+
+    public void SkillLearn(SkillType skillType)
+    {
+        SkillList.Add(skillType);
+    }
+}
